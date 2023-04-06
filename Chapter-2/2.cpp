a87335bfe4beaa428a036551490f23790620af6d
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-06 23:53:02 +0500

    Create 2.cpp

---
+++ Chapter-2/2.cpp
@@ -0,0 +1,14 @@
+/*Write a program that generates the following table:
+Use a single cout statement for all output.*/
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+	cout<<"1990"<<setw(8)<<setfill('.')<<"135";
+	cout<<"\n1991"<<setw(8)<<setfill('.')<<"7290";
+	cout<<"\n1992"<<setw(8)<<setfill('.')<<"11300";
+	cout<<"\n1993"<<setw(8)<<setfill('.')<<"16200";
+	return 0;
+  
+}
