b5772328ef0e1e93f2598ed7238e265890e4acde
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-06 23:51:39 +0500

    Create 1.cpp

---
+++ Chapter-2/1.cpp
@@ -0,0 +1,14 @@
+/*Assuming there are 7.481 gallons in a cubic foot, write a program that asks the user to
+enter a number of gallons, and then displays the equivalent in cubic feet.*/
+#include<iostream>
+
+using namespace std;
+int main()
+# define y 7.481
+{float x;
+cout<<"enter number of gallons ";
+
+cin>>x;
+cout<<"equivalent in cubic feet = "<<x/y;
+
+}
