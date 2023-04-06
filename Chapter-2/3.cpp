232b7f2ae08f4b26b80f1863b09c53a0eebe1cd3
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-06 23:56:45 +0500

    Create 3.cpp

---
+++ Chapter-2/3.cpp
@@ -0,0 +1,22 @@
+/*Write a program that generates the following output:
+10
+20
+19
+Use an integer constant for the 10, an arithmetic assignment operator to generate the 20,
+and a decrement operator to generate the 19.*/
+
+#include<iostream>
+using namespace std;
+int main(){
+	int x=10;
+	
+	 
+	cout<<x<<endl;  
+	x+=x;
+	cout<<x<<endl;
+	cout<<--x;   
+	
+    
+	
+	
+}
